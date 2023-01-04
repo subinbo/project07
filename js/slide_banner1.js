a911e776d645eb9ae8ee6970f7d67a14d2daa0e7
subinbo <121647154+subinbo@users.noreply.github.com>
2023-01-04 14:14:15 +0900

    Add files via upload

---
+++ js/slide_banner1.js
@@ -0,0 +1,50 @@
+﻿$(function(){
+    var visual = $('.best_wrap>ul>li'); //슬라이드 이미지
+    var leftBtn = $('.best_wrap .allow_left'); //왼쪽 버튼
+    var rightBtn = $('.best_wrap .allow_right'); //오른쪽 버튼
+    var current = 0 //배너이미지의 현재상태 초기화
+
+    /* 화살표 클릭 */
+    rightBtn.click(function(){
+        var prev = visual.eq(current);
+        move(prev, 0, '-100%');
+
+        current++;
+
+        if(current == visual.size()){current=0;}
+
+        var next = visual.eq(current);
+        move(next, '100%', 0);
+
+        return; //함수실행 종료
+    });
+
+    leftBtn.click(function(){
+        var prev = visual.eq(current);
+        move(prev, 0, '100%');
+
+        current--; //1씩 감소
+
+        if(current == -visual.size()){current=0;}
+
+        var next = visual.eq(current);
+        move(next, '-100%', 0);
+
+        return; //함수실행 종료
+    });
+
+
+
+
+    function move(tg, start, end){ //파라미터
+        tg.css('left',start).stop().animate({left:end},
+        {duration:500, ease:'easeOutCubic'}); //점점 빨라지게
+
+    }
+
+
+
+
+
+
+});
